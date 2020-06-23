e80cff0668a7695f024826f23b78b355d3cea2ec
houama <captainhoauam@gmail.com>
2020-06-23 15:55:57 +0700

    benerin css

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>BEM UMN</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    </head>
-    <body>
+@section('content')
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
@@ -29,7 +21,6 @@
 
             </div>
         </div>
-    </body>
     <script type="text/javascript" src="../js/app.js"></script>
     @extends('layouts.footer')
-</html>
+@endsection
