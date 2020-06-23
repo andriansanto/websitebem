c0d11819476738fce0f01f5ca15e1f975c4be1c8
andriansanto <58908029+andriansanto@users.noreply.github.com>
2020-06-23 18:48:29 +0700

    update

--- resources/js/components/Home.js
+++ resources/js/components/Home.js
@@ -1,58 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "~slick-carousel/slick/slick.css"; 
-import "~slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
-import { render } from 'sass';
-import '../css/reacthome.css';
 
-const photos = [
-    {
-        name: 'Photo 1',
-        url: '../../public/assets/img/img1.jpg'
-    },
-    {
-        name: 'Photo 2',
-        url: '../../public/assets/img/img2.jpg'
-    },
-    {
-        name: 'Photo 3',
-        url: '../../public/assets/img/img3.jpg'
-    }
-]
-
-
-
-function Home() {
-    render();{
-        const settings ={
-            dots: true,
-            fade: true,
-            infinite: true,
-            speed: 500,
-            slideToShow: 1,
-            arrows: true,
-            slideToSroll: 1,
-            className: "slides"
-        }
-    }
+function Example() {
     return (
-       <div className="Home">
-           <Slider {...settings}>
-               {photos.map((photo) => {
-                   return(
-                       <div>
-                           <img width="100%" src={photo.url} ></img>
-                       </div>
-                   )
-               })}
-           </Slider>
-       </div>
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <div className="card">
+                        <div className="card-header">Example Component</div>
+
+                        <div className="card-body">I'm an example component!</div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
-export default Home;
+export default Example;
 
 if (document.getElementById('root')) {
-    ReactDOM.render(<Home />, document.getElementById('root'));
+    ReactDOM.render(<Example />, document.getElementById('root'));
 }
