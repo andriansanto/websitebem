6c7bd0a0cc3f9dbc84e344902cfa274200ae0145
andriansanto <58908029+andriansanto@users.noreply.github.com>
2020-07-12 22:53:09 +0700

    Slider UKM + Logo

--- resources/js/components/Ukm.js
+++ resources/js/components/Ukm.js
@@ -1,22 +1,257 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import '../../css/reactukm.css';
+import '../../css/lightslider.css';
+import '../slidersukm/Jquery31';
+import '../slidersukm/lightslider';
+import '../slidersukm/sliderquery';
+
 
 class Ukm extends Component{
 render(){
+   
 return(
+<div className="allukm">
+        
     <div className="container-ukm">
-    <h1 className="ukm">UNIT KEGIATAN<br></br> MAHASISWA</h1>
-    <br></br>
-    <p className="textatasukm">
-    Universitas Multimedia Nusantara memiliki berbagai Unit Kegiatan Mahasiswa.<br></br>
-    (UKM) yang beragam. Yang dibagi menjadi 3 kategori yaitu, Seni dan Budaya, <br></br>
-    Sains dan Sosial, dan Olahraga. <br></br>
-    </p>          
-    </div>
+        <h1 className="ukm">UNIT KEGIATAN<br></br> MAHASISWA</h1>
+        <br></br>
+        <p className="textatasukm">
+        Universitas Multimedia Nusantara memiliki berbagai Unit Kegiatan Mahasiswa.<br></br>
+        (UKM) yang beragam. Yang dibagi menjadi 3 kategori yaitu, Seni dan Budaya, <br></br>
+        Sains dan Sosial, dan Olahraga. <br></br>
+        </p>          
+    </div>{/*Container ukm*/}
    
-   
-   
+    <div className="container-awal">
+
+        <div className="container-slider">
+            {/*Slider*/}
+            
+        <ul id="autoWidth" class="cs-hidden">
+
+            {/*1*/}
+            <li class="item-a">
+                
+                    {/*Slider Box*/}
+                <div className="box">
+                    <p className="senbud"></p>
+                    {/*model*/}
+                    <img src="assets/ukm/senbud/ulsonbig.png" className="model"></img>
+                    {/*details*/}
+                    
+                    <div className="details"> 
+                    {/*logo char*/}
+                    {/*<img src="#" className="logo" width="100px" style="hieght: auto;"></img>*/}
+                    {/*char details*/}
+                    <p>Ultima Sonora </p>
+                    </div>{/*details*/}
+
+                </div>{/*Box*/}
+
+            </li>{/*Ulson*/}
+
+            {/*2*/}
+            <li class="item-a">
+                
+                    {/*Slider Box*/}
+                <div className="box">
+                    <p className="senbud"></p>
+                    {/*model*/}
+                    <img src="assets/ukm/senbud/katakbig.png" className="model"></img>
+                    {/*details*/}
+                    
+                    <div className="details"> 
+                    {/*logo char*/}
+                    {/*<img src="#" className="logo" width="100px" style="hieght: auto;"></img>*/}
+                    {/*char details*/}
+                    <p>Teater KataK</p>
+                    </div>{/*details*/}
+
+                </div>{/*Box*/}
+
+            </li>{/*katak*/}
+
+            {/*3*/}
+            <li class="item-a">
+                
+                    {/*Slider Box*/}
+                <div className="box">
+                    <p className="senbud"></p>
+                    {/*model*/}
+                    <img src="assets/ukm/senbud/usobig.png" className="model"></img>
+                    {/*details*/}
+                    
+                    <div className="details"> 
+                    {/*logo char*/}
+                    {/*<img src="#" className="logo" width="100px" style="hieght: auto;"></img>*/}
+                    {/*char details*/}
+                    <p>UMN Symphony Orchestra</p>
+                    </div>{/*details*/}
+
+                </div>{/*Box*/}
+
+            </li>{/*Uso*/}
+
+            {/*4*/}
+            <li class="item-a">
+                
+                    {/*Slider Box*/}
+                <div className="box">
+                    <p className="senbud"></p>
+                    {/*model*/}
+                    <img src="assets/ukm/senbud/qoriebig.png" className="model"></img>
+                    {/*details*/}
+                    
+                    <div className="details"> 
+                    {/*logo char*/}
+                    {/*<img src="#" className="logo" width="100px" style="hieght: auto;"></img>*/}
+                    {/*char details*/}
+                    <p>Qorie</p>
+                    </div>{/*details*/}
+
+                </div>{/*Box*/}
+
+            </li>{/*qorie*/}
+
+            {/*5*/}
+            <li class="item-a">
+                
+                    {/*Slider Box*/}
+                <div className="box">
+                    <p className="senbud"></p>
+                    {/*model*/}
+                    <img src="assets/ukm/senbud/jcafebig.png" className="model"></img>
+                    {/*details*/}
+                    
+                    <div className="details"> 
+                    {/*logo char*/}
+                    {/*<img src="#" className="logo" width="100px" style="hieght: auto;"></img>*/}
+                    {/*char details*/}
+                    <p>J-Cafe</p>
+                    </div>{/*details*/}
+
+                </div>{/*Box*/}
+
+            </li>{/*jcafe*/}
+
+            {/*6*/}
+            <li class="item-a">
+                
+                    {/*Slider Box*/}
+                <div className="box">
+                    <p className="senbud"></p>
+                    {/*model*/}
+                    <img src="assets/ukm/senbud/mufomicbig.png" className="model"></img>
+                    {/*details*/}
+                    
+                    <div className="details"> 
+                    {/*logo char*/}
+                    {/*<img src="#" className="logo" width="100px" style="hieght: auto;"></img>*/}
+                    {/*char details*/}
+                    <p>Mufomic</p>
+                    </div>{/*details*/}
+
+                </div>{/*Box*/}
+
+            </li>{/*mufomic*/}
+
+            {/*7*/}
+            <li class="item-a">
+                
+                    {/*Slider Box*/}
+                <div className="box">
+                    <p className="senbud"></p>
+                    {/*model*/}
+                    <img src="assets/ukm/senbud/obscurabig.png" className="model"></img>
+                    {/*details*/}
+                    
+                    <div className="details"> 
+                    {/*logo char*/}
+                    {/*<img src="#" className="logo" width="100px" style="hieght: auto;"></img>*/}
+                    {/*char details*/}
+                    <p>Obscura</p>
+                    </div>{/*details*/}
+
+                </div>{/*Box*/}
+
+            </li>{/*obscura*/}
+
+            {/*8*/}
+            <li class="item-a">
+                
+                    {/*Slider Box*/}
+                <div className="box">
+                    <p className="senbud"></p>
+                    {/*model*/}
+                    <img src="assets/ukm/senbud/traccebig.png" className="model"></img>
+                    {/*details*/}
+                    
+                    <div className="details"> 
+                    {/*logo char*/}
+                    {/*<img src="#" className="logo" width="100px" style="hieght: auto;"></img>*/}
+                    {/*char details*/}
+                    <p>Tracce</p>
+                    </div>{/*details*/}
+
+                </div>{/*Box*/}
+
+            </li>{/*tracce*/}
+
+            {/*9*/}
+            <li class="item-a">
+                
+                    {/*Slider Box*/}
+                <div className="box">
+                    <p className="senbud"></p>
+                    {/*model*/}
+                    <img src="assets/ukm/senbud/utoysbig.png" className="model"></img>
+                    {/*details*/}
+                    
+                    <div className="details"> 
+                    {/*logo char*/}
+                    {/*<img src="#" className="logo" width="100px" style="hieght: auto;"></img>*/}
+                    {/*char details*/}
+                    <p>Ultima Toys</p>
+                    </div>{/*details*/}
+
+                </div>{/*Box*/}
+
+            </li>{/*utoys*/}
+
+            {/*10*/}
+            <li class="item-a">
+                
+                    {/*Slider Box*/}
+                <div className="box">
+                    <p className="senbud"></p>
+                    {/*model*/}
+                    <img src="assets/ukm/senbud/streetdancebig.png" className="model"></img>
+                    {/*details*/}
+                    
+                    <div className="details"> 
+                    {/*logo char*/}
+                    {/*<img src="#" className="logo" width="100px" style="hieght: auto;"></img>*/}
+                    {/*char details*/}
+                    <p>Street Dance</p>
+                    </div>{/*details*/}
+
+                </div>{/*Box*/}
+
+            </li>{/*Street Dance*/}
+
+        </ul>
+
+            
+            
+
+        </div>{/*container slider*/}
+
+    </div>{/*Containerawal*/}
+
+    
+
+</div>/*End allukm*/
    );
 }
 }
