753ff4057c81b96a8c4e0f64f055648013ea71b3
Steven Johan <steven.johan@student.umn.ac.id>
2020-07-28 00:54:48 +0700

    edit himpunan

--- resources/js/components/Himpunan.js
+++ resources/js/components/Himpunan.js
@@ -6,15 +6,35 @@ import '../../css/reacthimp.css';
 class Himpunan extends Component{
 render(){
 return(
-    <div className="container-himp">
-    <h1 className="himp-maha">HIMPUNAN <br></br> MAHASISWA</h1>
-    <br></br>
-    <p>
-    Universitas Multimedia Nusantara terbagi menjadi 4 fakultas besar.<br></br>
-    Keempat fakultas terbagi menjadi beberapa jurusan dengan dinaungi oleh <br></br>
-    himpunan jurusan tersebut. Berikut merupakan ke 11 himpunan yang terdapat <br></br>
-    pada Universitas Multimedia Nusantara
-    </p>          
+    <div>
+        <div className="container-himp">
+            <h1 className="himp-maha">HIMPUNAN <br></br> MAHASISWA</h1>
+            <br></br>
+            <p>
+            Universitas Multimedia Nusantara terbagi menjadi 4 fakultas besar.<br></br>
+            Keempat fakultas terbagi menjadi beberapa jurusan dengan dinaungi oleh <br></br>
+            himpunan jurusan tersebut. Berikut merupakan ke 11 himpunan yang terdapat <br></br>
+            pada Universitas Multimedia Nusantara
+            </p>          
+        </div>
+        <Container>
+            <Row>
+                <Col className="card heads">FAKULTAS</Col>
+                <Col className="card">
+                    <div className="fakultas-title">ILMU KOMUNITAS</div>
+                    <img src="../../public/assets/himp/ilkom.jpg"></img>
+                </Col>
+                <Col className="card">
+                    <div className="fakultas-title">SENI DAN DESAIN</div>
+                </Col>
+                <Col className="card">
+                    <div className="fakultas-title">TEKNIK INFORMATIKA</div>
+                </Col>
+                <Col className="card">
+                    <div className="fakultas-title">BISNIS</div>
+                </Col>
+            </Row>
+        </Container>
     </div>
     );
 }
