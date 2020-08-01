13ec6668e16ebbd2c7f798b45b58389cab613190
houama <captainhoauam@gmail.com>
2020-08-01 19:28:55 +0700

    update keg

--- resources/js/components/KegiatanMahasiswa.js
+++ resources/js/components/KegiatanMahasiswa.js
@@ -22,18 +22,45 @@ $(document).ready(function() {
     });
 }
 
+function changeTab(){
+    var radio = document.getElementsByClassName("radio");
+    console.log(radio.length);
+    console.log("test");
+        if(document.getElementsByClassName("radio").value == 'keg'){
+            document.getElementsByClassName("text-tab-pane").style.cssText = "background-image: linear-gradient(#ffb359,#ffffff);"
+            console.log("pertama");
+        }
+        else if(document.getElementsByClassName("radio").value == 'lso'){
+            document.getElementsByClassName("text-tab-pane").style.cssText = "background-image: linear-gradient(#ade2b2,#ffffff);"
+            console.log("kedua");
+        }
+
+}
+
 class KegiatanMahasiswa extends Component{
     constructor() {
         super();
         this.state = { tabIndex: 0 };
     }
+
+    // onCheckRadio(e){
+    //     console.log(e.target.value);
+
+    //     this.setState({
+    //         [e.target.name]: e.target.checked
+    //     })
+    // }
     render(){
         const displayTabs = (
             <Tabs selectedIndex={this.state.tabIndex} onSelect={tabIndex => this.setState({ tabIndex })} onClick={slider()}>
             <TabList>
-            <Tab className="text-tab-pane">Kegiatan<br></br>Mahasiswa</Tab>
-            <Tab className="text-tab-pane">Lembaga<br></br>Semi Otonom</Tab>
+            <div id="wrap">
+            <Tab className="text-tab-pane"><input type="radio" className="radio" value="keg" onClick={changeTab()}></input>Kegiatan<br></br>Mahasiswa</Tab>
+            <Tab className="text-tab-pane"><input type="radio" className="radio" value="lso" onClick={changeTab()}></input>Lembaga<br></br>Semi Otonom</Tab>
+            </div>
             </TabList>
+
+            {/*TAB PAN KEGIATAN*/}
 
             <TabPanel>
             <div className="container-awal">
@@ -49,8 +76,12 @@ class KegiatanMahasiswa extends Component{
                     {/*Slider Box*/}
                 <div className="box">
                     {/*model*/}
+                    <Row>
+                    <div className="col-md-5">
                     <img src="assets/kegiatan/logoufestbig.png" className="model-keg"></img>
-                    <Container><p className="title">U-FEST</p></Container>
+                    </div>
+                    <div className="col">
+                    <p className="title">U-FEST</p>
 
                     {/*Penjelasan*/}
                     <div className="info">
@@ -59,6 +90,8 @@ class KegiatanMahasiswa extends Component{
 
                     <a href="instagram.com/bemumn">@ultimasonora</a>
                     </div>
+                    </div>
+                    </Row>
 
                     {/*details*/}
                     
@@ -71,15 +104,19 @@ class KegiatanMahasiswa extends Component{
 
                 </div>{/*Box*/}
 
-            </li>{/*Ulson*/}
+            </li>{/*U-fest*/}
 
-            {/*1*/}
+            {/*2*/}
             <li className="item-a">
                 
                     {/*Slider Box*/}
                 <div className="box">
                     {/*model*/}
+                    <Row>
+                    <div className="col-md-5">
                     <img src="assets/kegiatan/logomaximabig.png" className="model-keg"></img>
+                    </div>
+                    <div className="col">
                     <p className="title">MAXIMA</p>
 
                     {/*Penjelasan*/}
@@ -89,6 +126,8 @@ class KegiatanMahasiswa extends Component{
 
                     <a href="instagram.com/bemumn">@ultimasonora</a>
                     </div>
+                    </div>
+                    </Row>
 
                     {/*details*/}
                     
@@ -101,15 +140,19 @@ class KegiatanMahasiswa extends Component{
 
                 </div>{/*Box*/}
 
-            </li>{/*Ulson*/}
+            </li>{/*Maxima*/}
 
-            {/*1*/}
+            {/*3*/}
             <li className="item-a">
                 
                     {/*Slider Box*/}
                 <div className="box">
                     {/*model*/}
+                    <Row>
+                    <div className="col-md-4">
                     <img src="assets/kegiatan/logomrmsbig.png" className="model-mrms"></img>
+                    </div>
+                    <div className="col">
                     <p className="title">MR. & MS. UMN</p>
 
                     {/*Penjelasan*/}
@@ -119,6 +162,8 @@ class KegiatanMahasiswa extends Component{
 
                     <a href="instagram.com/bemumn">@ultimasonora</a>
                     </div>
+                    </div>
+                    </Row>
 
                     {/*details*/}
                     
@@ -131,19 +176,127 @@ class KegiatanMahasiswa extends Component{
 
                 </div>{/*Box*/}
 
-            </li>{/*Ulson*/}
+            </li>{/*Mr. & Ms. UMN*/}
+
+            {/*4*/}
+            <li className="item-a">
+                
+                    {/*Slider Box*/}
+                <div className="box">
+                    {/*model*/}
+                    <Row>
+                    <div className="col-md-5">
+                    <img src="assets/kegiatan/logostarlightbig.png" className="model-keg"></img>
+                    </div>
+                    <div className="col">
+                    <p className="title">STARLIGHT</p>
+
+                    {/*Penjelasan*/}
+                    <div className="info">
+                    <p>Ultima Sonora adalah unit kegiatan mahasiswa</p>
+                    <p>yang bergerak dalam bidang paduan suara</p>
+
+                    <a href="instagram.com/bemumn">@ultimasonora</a>
+                    </div>
+                    </div>
+                    </Row>
+
+                    {/*details*/}
+                    
+                    <div className="details"> 
+                    {/*logo char*/}
+                    {/*<img src="#" className="logo" width="100px" style="hieght: auto;"></img>*/}
+                    {/*char details*/}
+                    <p>Ultima Sonora </p>
+                    </div>{/*details*/}
+
+                </div>{/*Box*/}
+
+            </li>{/*Starlight*/}
+
+            {/*5*/}
+            <li className="item-a">
+                
+                    {/*Slider Box*/}
+                <div className="box">
+                    {/*model*/}
+                    <Row>
+                    <div className="col-md-5">
+                    <img src="assets/kegiatan/logobanksampahbig.png" className="model-keg"></img>
+                    </div>
+                    <div className="col">
+                    <p className="title">BANK SAMPAH</p>
+
+                    {/*Penjelasan*/}
+                    <div className="info">
+                    <p>Ultima Sonora adalah unit kegiatan mahasiswa</p>
+                    <p>yang bergerak dalam bidang paduan suara</p>
+
+                    <a href="instagram.com/bemumn">@ultimasonora</a>
+                    </div>
+                    </div>
+                    </Row>
+
+                    {/*details*/}
+                    
+                    <div className="details"> 
+                    {/*logo char*/}
+                    {/*<img src="#" className="logo" width="100px" style="hieght: auto;"></img>*/}
+                    {/*char details*/}
+                    <p>Ultima Sonora </p>
+                    </div>{/*details*/}
+
+                </div>{/*Box*/}
+
+            </li>{/*Bank Sampah*/}
+
+            {/*6*/}
+            <li className="item-a">
+                
+                    {/*Slider Box*/}
+                <div className="box">
+                    {/*model*/}
+                    <Row>
+                    <div className="col-md-5">
+                    <img src="assets/kegiatan/logoombbig.png" className="model-keg"></img>
+                    </div>
+                    <div className="col">
+                    <p className="title">OMB UMN</p>
+
+                    {/*Penjelasan*/}
+                    <div className="info">
+                    <p>Ultima Sonora adalah unit kegiatan mahasiswa</p>
+                    <p>yang bergerak dalam bidang paduan suara</p>
+
+                    <a href="instagram.com/bemumn">@ultimasonora</a>
+                    </div>
+                    </div>
+                    </Row>
+
+                    {/*details*/}
+                    
+                    <div className="details"> 
+                    {/*logo char*/}
+                    {/*<img src="#" className="logo" width="100px" style="hieght: auto;"></img>*/}
+                    {/*char details*/}
+                    <p>Ultima Sonora </p>
+                    </div>{/*details*/}
+
+                </div>{/*Box*/}
+
+            </li>{/*OMB*/}
 
             </ul>
-
-            
-            
 
         </div>{/*container slider*/}
 
     </div>{/*Containerawal*/}
-            </TabPanel>
-            <TabPanel>
-            <div className="container-awal">
+    </TabPanel>
+
+    {/*TAB PAN LSO*/}
+
+<TabPanel>
+<div className="container-awal">
 
 <div className="container-slider">
     {/*Slider*/}
@@ -155,17 +308,23 @@ class KegiatanMahasiswa extends Component{
         
             {/*Slider Box*/}
         <div className="box">
-            <p className="senbud">Ultima Sonora</p>
             {/*model*/}
-            <img src="assets/ukmnobg/senbud/ulsonbg.png" className="model"></img>
+            <Row>
+                    <div className="col-md-5">
+                    <img src="assets/lso/logorumpinbig.png" className="model-lso-box"></img>
+                    </div>
+                    <div className="col">
+                    <p className="title">RUMPIN</p>
 
-            {/*Penjelasan*/}
-            <div className="info">
-            <p>Ultima Sonora adalah unit kegiatan mahasiswa</p>
-            <p>yang bergerak dalam bidang paduan suara</p>
+                    {/*Penjelasan*/}
+                    <div className="info">
+                    <p>Ultima Sonora adalah unit kegiatan mahasiswa</p>
+                    <p>yang bergerak dalam bidang paduan suara</p>
 
-            <a href="instagram.com/bemumn">@ultimasonora</a>
-            </div>
+                    <a href="instagram.com/bemumn">@ultimasonora</a>
+                    </div>
+                    </div>
+                    </Row>
 
             {/*details*/}
             
@@ -178,24 +337,30 @@ class KegiatanMahasiswa extends Component{
 
         </div>{/*Box*/}
 
-    </li>{/*Ulson*/}
+    </li>{/*Rumpin*/}
 
-    {/*1*/}
+    {/*2*/}
     <li className="item-a">
         
             {/*Slider Box*/}
         <div className="box">
-            <p className="senbud">Ultima Sonora</p>
             {/*model*/}
-            <img src="assets/ukmnobg/senbud/ulsonbg.png" className="model"></img>
+            <Row>
+                    <div className="col-md-5">
+                    <img src="assets/lso/logoubizbig.png" className="model-lso-box"></img>
+                    </div>
+                    <div className="col">
+                    <p className="title">U-BIZ</p>
 
-            {/*Penjelasan*/}
-            <div className="info">
-            <p>Ultima Sonora adalah unit kegiatan mahasiswa</p>
-            <p>yang bergerak dalam bidang paduan suara</p>
+                    {/*Penjelasan*/}
+                    <div className="info">
+                    <p>Ultima Sonora adalah unit kegiatan mahasiswa</p>
+                    <p>yang bergerak dalam bidang paduan suara</p>
 
-            <a href="instagram.com/bemumn">@ultimasonora</a>
-            </div>
+                    <a href="instagram.com/bemumn">@ultimasonora</a>
+                    </div>
+                    </div>
+                    </Row>
 
             {/*details*/}
             
@@ -208,24 +373,30 @@ class KegiatanMahasiswa extends Component{
 
         </div>{/*Box*/}
 
-    </li>{/*Ulson*/}
+    </li>{/*U-Biz*/}
 
-    {/*1*/}
+    {/*3*/}
     <li className="item-a">
         
             {/*Slider Box*/}
         <div className="box">
-            <p className="senbud">Ultima Sonoraaaaaa</p>
             {/*model*/}
-            <img src="assets/ukmnobg/senbud/ulsonbg.png" className="model"></img>
+            <Row>
+                    <div className="col-md-4">
+                    <img src="assets/lso/logodunarbig.png" className="model-lso-dunar"></img>
+                    </div>
+                    <div className="col">
+                    <p className="title">DUTA ANTI NARKOBA</p>
 
-            {/*Penjelasan*/}
-            <div className="info">
-            <p>Ultima Sonora adalah unit kegiatan mahasiswa</p>
-            <p>yang bergerak dalam bidang paduan suara</p>
+                    {/*Penjelasan*/}
+                    <div className="info">
+                    <p>Ultima Sonora adalah unit kegiatan mahasiswa</p>
+                    <p>yang bergerak dalam bidang paduan suara</p>
 
-            <a href="instagram.com/bemumn">@ultimasonora</a>
-            </div>
+                    <a href="instagram.com/bemumn">@ultimasonora</a>
+                    </div>
+                    </div>
+                    </Row>
 
             {/*details*/}
             
@@ -238,7 +409,116 @@ class KegiatanMahasiswa extends Component{
 
         </div>{/*Box*/}
 
-    </li>{/*Ulson*/}
+    </li>{/*Dunar*/}
+
+
+    {/*4*/}
+    <li className="item-a">
+        
+            {/*Slider Box*/}
+        <div className="box">
+            {/*model*/}
+            <Row>
+                    <div className="col-md-5">
+                    <img src="assets/lso/logomedicbig.png" className="model-lso-medic"></img>
+                    </div>
+                    <div className="col">
+                    <p className="title">UMN MEDIC</p>
+
+                    {/*Penjelasan*/}
+                    <div className="info">
+                    <p>Ultima Sonora adalah unit kegiatan mahasiswa</p>
+                    <p>yang bergerak dalam bidang paduan suara</p>
+
+                    <a href="instagram.com/bemumn">@ultimasonora</a>
+                    </div>
+                    </div>
+                    </Row>
+
+            {/*details*/}
+            
+            <div className="details"> 
+            {/*logo char*/}
+            {/*<img src="#" className="logo" width="100px" style="hieght: auto;"></img>*/}
+            {/*char details*/}
+            <p>Ultima Sonora </p>
+            </div>{/*details*/}
+
+        </div>{/*Box*/}
+
+    </li>{/*Medic*/}
+
+    {/*5*/}
+    <li className="item-a">
+        
+            {/*Slider Box*/}
+        <div className="box">
+            {/*model*/}
+            <Row>
+                    <div className="col-md-5">
+                    <img src="assets/lso/logokomcorbig.png" className="model-lso-komcor"></img>
+                    </div>
+                    <div className="col">
+                    <p className="title">KOMPAS CORNER</p>
+
+                    {/*Penjelasan*/}
+                    <div className="info">
+                    <p>Ultima Sonora adalah unit kegiatan mahasiswa</p>
+                    <p>yang bergerak dalam bidang paduan suara</p>
+
+                    <a href="instagram.com/bemumn">@ultimasonora</a>
+                    </div>
+                    </div>
+                    </Row>
+
+            {/*details*/}
+            
+            <div className="details"> 
+            {/*logo char*/}
+            {/*<img src="#" className="logo" width="100px" style="hieght: auto;"></img>*/}
+            {/*char details*/}
+            <p>Ultima Sonora </p>
+            </div>{/*details*/}
+
+        </div>{/*Box*/}
+
+    </li>{/*Komcor*/}
+
+    {/*6*/}
+    <li className="item-a">
+        
+            {/*Slider Box*/}
+        <div className="box">
+            {/*model*/}
+            <Row>
+                    <div className="col-md-5">
+                    <img src="assets/lso/logoustorebig.png" className="model-lso-ustore"></img>
+                    </div>
+                    <div className="col">
+                    <p className="title">U-STORE</p>
+
+                    {/*Penjelasan*/}
+                    <div className="info">
+                    <p>Ultima Sonora adalah unit kegiatan mahasiswa</p>
+                    <p>yang bergerak dalam bidang paduan suara</p>
+
+                    <a href="instagram.com/bemumn">@ultimasonora</a>
+                    </div>
+                    </div>
+                    </Row>
+
+            {/*details*/}
+            
+            <div className="details"> 
+            {/*logo char*/}
+            {/*<img src="#" className="logo" width="100px" style="hieght: auto;"></img>*/}
+            {/*char details*/}
+            <p>Ultima Sonora </p>
+            </div>{/*details*/}
+
+        </div>{/*Box*/}
+
+    </li>{/*U-Store*/}
 
     </ul>
 
@@ -260,17 +540,19 @@ return(
 }
 }
 
-function tabkegiatan(){
-    $("#react-tabs-0").click(function(){
-        $('#autoWidth').lightSlider({
-            autoWidth:true,
-            loop:true,
-            onSliderLoad: function() {
-              $('#autoWidth').removeClass('cs-hidden');
-          } 
-       });
-    });
-  }
+// $( document ).ready(function() {
+// var wrapper = document.getElementById("wrap");
+// var tabs = wrapper.getElementsByClassName("page");
+// console.log(tabs.length);
+// for (var i = 0; i < tabs.length; i++) {
+//   tabs[i].addEventListener("click", function() {
+//   var current = document.getElementsByClassName("active");
+//   current[0].className = current[0].className.replace(" active", "");
+//   this.className += " active";
+//   });
+// }
+// });
+
 
 export default KegiatanMahasiswa;
 
